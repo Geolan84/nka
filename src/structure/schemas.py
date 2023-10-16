feb0fdc728096e74dc2e11e5476163ee09d17913
Lanin George <gmlanin@edu.hse.ru>
2023-10-17 01:43:51 +0300

    Removed qualification and profession from schema.

--- src/structure/schemas.py
+++ src/structure/schemas.py
@@ -7,9 +7,6 @@ class NewUser(BaseModel):
     second_name: str
     patronymic: Optional[str] = None
     department_id: Optional[int] = None
-    qual_id: Optional[int] = None
-    prof_id: Optional[int] = None
-    #head_id: Optional[int] = None
     is_head: bool
 
 class PatchUser(BaseModel):
@@ -17,7 +14,4 @@ class PatchUser(BaseModel):
     first_name: Optional[str] = None
     second_name: Optional[str] = None
     patronymic: Optional[str] = None
-    qual_id: Optional[int] = None
-    prof_id: Optional[int] = None
-    head_id: Optional[int] = None
     is_head: Optional[bool] = None
