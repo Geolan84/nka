bd89b9fab861baaeea03deb883a7eef2736ff750
Lanin George <gmlanin@edu.hse.ru>
2023-10-23 03:53:19 +0300

    Added GET 'heads' for app.

--- server/src/structure/router.py
+++ server/src/structure/router.py
@@ -7,46 +7,42 @@ from structure.struct_repository import StructRepository as struct
 from structure.schemas import NewUser, PatchUser
 
 
+router = APIRouter(prefix="/struct", tags=["Structure"])
 
-router = APIRouter(
-    prefix="/struct",
-    tags=["Structure"]
-)
 
 @router.post("/new_department", status_code=201)
 async def add_department(department_name: str, head_department_id: int | None = None):
     department_error = await struct.add_department(department_name, head_department_id)
     if department_error is not None:
         raise HTTPException(status_code=400, detail=str(department_error))
-    
-    
+
+
 @router.patch("/edit_department", status_code=200)
 async def edit_department(new_name: str, department_id: int):
     department_error = await struct.edit_department(department_id, new_name)
     if department_error is not None:
         raise HTTPException(status_code=400, detail=department_error)
-    
+
 
 @router.get("/get_all_departments", status_code=200)
 async def get_all_departments(token: HTTPAuthorizationCredentials = Security(bearer)):
     info = read_token(token)
     if info is None:
         raise HTTPException(
-            status_code=401,
-            detail="Not authorized. Use /auth/login endpoint."
+            status_code=401, detail="Not authorized. Use /auth/login endpoint."
         )
-    role = info.get('role')
+    role = info.get("role")
     if role is None or role < 2:
         raise HTTPException(
-            status_code=403,
-            detail="Forbidden. You are not head or admin."
+            status_code=403, detail="Forbidden. You are not head or admin."
         )
     try:
         res = await struct.get_all_departments()
         return res
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
-    
+
+
 @router.get("/get_department", status_code=200)
 async def get_department(department_id: int | None = None):
     try:
@@ -54,27 +50,42 @@ async def get_department(department_id: int | None = None):
         return res
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
-    
-    
-    
+
+
 @router.delete("/delete_department", status_code=200)
 async def delete_department(department_id: int):
     department_error = await struct.delete_department(department_id)
     if department_error is not None:
         raise HTTPException(status_code=400, detail=department_error)
 
+
 @router.post("/add_user", status_code=201)
 async def add_user(new_user: NewUser):
     add_error = await struct.add_new_user(dict(new_user))
     if add_error is not None:
         raise HTTPException(status_code=400, detail=str(add_error))
-    
+
+
 @router.get("/get_user", status_code=200)
 async def add_user(user_id: int):
     try:
         return await struct.get_user(user_id)
     except Exception as e:
         raise HTTPException(status_code=400, detail="No user with this ID")
+
+
+@router.get("/heads", status_code=200)
+async def get_heads(token: HTTPAuthorizationCredentials = Security(bearer)):
+    info = read_token(token)
+    if info is None:
+        raise HTTPException(
+            status_code=401, detail="Not authorized. Use /auth/login endpoint."
+        )
+    heads = await struct.get_heads()
+    if heads is None:
+        raise HTTPException(status_code=400, detail="Some problems with heads.")
+    return heads
+
 
 # @router.patch("/edit_user", status_code=200)
 # async def edit_user(patch_user: PatchUser):
--- server/src/structure/struct_repository.py
+++ server/src/structure/struct_repository.py
@@ -1,13 +1,15 @@
 import asyncpg
 from config import DATABASE_URL, Role
 
-class StructRepository:
 
+class StructRepository:
     @staticmethod
     async def add_department(name: str, head_id: int | None):
         try:
             connection = await asyncpg.connect(DATABASE_URL)
-            await connection.execute('insert into department values(default, $1, $2);', name, head_id)
+            await connection.execute(
+                "insert into department values(default, $1, $2);", name, head_id
+            )
         except Exception as e:
             return e
         finally:
@@ -17,7 +19,11 @@ class StructRepository:
     async def edit_department(department_id: int, new_name: str):
         try:
             connection = await asyncpg.connect(DATABASE_URL)
-            res = await connection.execute('update department set department_name=$1 where department_id=$2;', new_name, department_id)
+            res = await connection.execute(
+                "update department set department_name=$1 where department_id=$2;",
+                new_name,
+                department_id,
+            )
             if res[-1] == "0":
                 return "No department with this ID."
         except Exception as e:
@@ -29,19 +35,26 @@ class StructRepository:
     async def delete_department(department_id: int):
         try:
             connection = await asyncpg.connect(DATABASE_URL)
-            sub_dep_count = await connection.fetchval("""
+            sub_dep_count = await connection.fetchval(
+                """
                                                         select count(*) from department
-                                                        where head_department_id = $1;""", 
-                                                        department_id)
+                                                        where head_department_id = $1;""",
+                department_id,
+            )
             if sub_dep_count != 0:
                 return "This department has children department(s)!"
-            sub_users_count = await connection.fetchval("""
+            sub_users_count = await connection.fetchval(
+                """
                                                         select count(*) from users
                                                         where department_id = $1;
-                                                        """, department_id)
+                                                        """,
+                department_id,
+            )
             if sub_users_count != 0:
                 return "This department has employees!"
-            res = await connection.execute('delete from department where department_id=$1;', department_id)
+            res = await connection.execute(
+                "delete from department where department_id=$1;", department_id
+            )
             if res[-1] == "0":
                 return "No department with this ID."
         except Exception as e:
@@ -64,6 +77,7 @@ class StructRepository:
                     departments_map[dep.get("head_department_id")] = [dict(dep)]
             if None not in departments_map:
                 raise Exception("Departments doesn't contain root!")
+
             # ToDo: Требуется профилирование!
             def __recursive_build(key: int):
                 if key in departments_map:
@@ -71,7 +85,8 @@ class StructRepository:
                     for y in layer:
                         y["departments"] = __recursive_build(y["department_id"])
                     return layer
-            # Alternative version.  
+
+            # Alternative version.
             # def __recursive_build(key: int):
             #     if key in departments_map:
             #         layer = departments_map[key]
@@ -91,21 +106,31 @@ class StructRepository:
             answer = {}
             connection = await asyncpg.connect(DATABASE_URL)
             if department_id is None:
-                current_department = await connection.fetchrow('select * from department where head_department_id is null;')
+                current_department = await connection.fetchrow(
+                    "select * from department where head_department_id is null;"
+                )
             else:
-                current_department = await connection.fetchrow('select * from department where department_id = $1;', department_id)
+                current_department = await connection.fetchrow(
+                    "select * from department where department_id = $1;", department_id
+                )
             if current_department is None:
                 raise Exception("No department here!")
-             
-            users = await connection.fetch("""
+
+            users = await connection.fetch(
+                """
                     select user_id, email, first_name, second_name, patronymic, department_id, department_name, role_id
                     from users join users_role using(user_id) join department using(department_id)
-                    where department_id=$1;""", current_department.get('department_id'))
-            
-            departments = await connection.fetch('select * from department where head_department_id=$1;', current_department.get('department_id'))
+                    where department_id=$1;""",
+                current_department.get("department_id"),
+            )
 
-            answer["department_name"] = current_department.get('department_name')
-            answer["department_id"] = current_department.get('department_id')
+            departments = await connection.fetch(
+                "select * from department where head_department_id=$1;",
+                current_department.get("department_id"),
+            )
+
+            answer["department_name"] = current_department.get("department_name")
+            answer["department_id"] = current_department.get("department_id")
             answer["departments"] = [dict(x) for x in departments]
             answer["users"] = [dict(x) for x in users]
             return answer
@@ -120,12 +145,47 @@ class StructRepository:
         try:
             connection = await asyncpg.connect(DATABASE_URL)
             async with connection.transaction():
-                new_user_id = await connection.fetchval('insert into users values(default, $1, $2, $3, $4, $5, $6) returning user_id;', 
-                                                       user.get('email'), 'ТУТПАРОЛЬ', user.get('first_name'),
-                                                       user.get('second_name'), user.get('patronymic'), user.get('department_id'))
-                await connection.execute('insert into users_role values($1, $2);', new_user_id, Role.HEAD.value if user.get('is_head') else Role.EMPLOYEE.value)
+                new_user_id = await connection.fetchval(
+                    "insert into users values(default, $1, $2, $3, $4, $5, $6) returning user_id;",
+                    user.get("email"),
+                    "ТУТПАРОЛЬ",
+                    user.get("first_name"),
+                    user.get("second_name"),
+                    user.get("patronymic"),
+                    user.get("department_id"),
+                )
+                await connection.execute(
+                    "insert into users_role values($1, $2);",
+                    new_user_id,
+                    Role.HEAD.value if user.get("is_head") else Role.EMPLOYEE.value,
+                )
         except Exception as e:
             return e
+        finally:
+            await connection.close()
+
+    @staticmethod
+    async def get_heads():
+        try:
+            connection = await asyncpg.connect(DATABASE_URL)
+            heads = await connection.fetch(
+                """
+                                    select user_id, first_name, second_name, patronymic from users join users_role using(user_id) where role_id = $1 order by second_name;
+                                   """,
+                Role.HEAD.value,
+            )
+            if heads is None:
+                return []
+            answer = []
+            for x in heads:
+                full_name = f'{x.get("second_name")} {x.get("first_name")} {x.get("patronymic")}'.replace(
+                    "None", ""
+                ).rstrip()
+                answer.append({"user_id": x.get("user_id"), "full_name": full_name})
+
+            return {"heads": answer}
+        except Exception as e:
+            print(e)
         finally:
             await connection.close()
 
@@ -133,18 +193,42 @@ class StructRepository:
     async def get_user(user_id: int):
         try:
             connection = await asyncpg.connect(DATABASE_URL)
-            user = await connection.fetchrow("""
-            select user_id, email, first_name, second_name, patronymic, department_id, department_name, role_id
-            from users join users_role using(user_id) join department using(department_id)
-            where user_id=$1;
-            """, user_id)
-            return dict(user)
+            user = await connection.fetchrow(
+                """
+                                                select
+                                                    user_id,
+                                                    email,
+                                                    first_name,
+                                                    second_name,
+                                                    patronymic,
+                                                    role_id,
+                                                    department_id
+                                                from
+                                                    users
+                                                join users_role
+                                                        using(user_id)
+                                                where
+                                                    user_id =$1;""",
+                user_id,
+            )
+            if user is not None:
+                user = dict(user)
+                if user.get("department_id") is None:
+                    user["department_name"] = "-"
+                else:
+                    user["department_name"] = await connection.fetchval(
+                        """
+                                                                        select department_name from department
+                                                                        where department_id = $1;
+                                                                        """,
+                        user["department_id"],
+                    )
+                return user
         except Exception as e:
             print(e)
             raise e
         finally:
             await connection.close()
-
 
     # @staticmethod
     # async def edit_user(user: dict):
@@ -155,12 +239,12 @@ class StructRepository:
 
     #         #'UPDATE users SET a=b, a=v WHERE email = 'Drama';'
     #         # async with connection.transaction():
-    #         #     new_user_id = await connection.fetchval('insert into users values(default, $1, $2, $3, $4, $5, $6, $7) returning user_id;', 
+    #         #     new_user_id = await connection.fetchval('insert into users values(default, $1, $2, $3, $4, $5, $6, $7) returning user_id;',
     #         #                                            user.get('email'), user.get('password'), user.get('first_name'),
     #         #                                            user.get('second_name'), user.get('patronymic'),
     #         #                                            user.get('qual_id'), user.get('prof_id'))
     #         #     await connection.execute('insert into users_role values($1, $2);', new_user_id, Role.HEAD.value if user.get('is_head') else Role.EMPLOYEE.value)
-            
+
     #     except Exception as e:
     #         return e
     #     finally:
